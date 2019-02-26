bab787606f27c85c50577288226ab49bf5df2abf
TarzanRoboticsMan <dsontag00@stuy.edu>
2019-02-26 11:01:28 -0500

    finished classwork, yet t odebug or test

--- Maze.java
+++ Maze.java
@@ -7,6 +7,21 @@ public class Maze{
     try{
       File text = new File("input.txt");
       Scanner inf = new Scanner(text);
+
+      ArrayList<String> data = new ArrayList<String>;
+      while(inf.hasNextLine()){
+        String line = inf.nextLine();
+        data.add(line);
+        System.out.println(line);
+      }
+
+      Char[][] maze = new Char[data.length][data[0].length];
+      for(int l=0;l<data.length;l++){
+        for(int x=0;x<data[l].length();x++){
+          maze[l][c] = data[l].charAt(c);
+        }
+      }
+      System.out.println(maze);
     }
     catch(FileNotFoundException E){
       System.out.println("I can't find your file");
