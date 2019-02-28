14ade90b6b1e381cdc6a8632d718512ac396a3c7
TarzanRoboticsMan <dsontag00@stuy.edu>
2019-02-28 10:59:03 -0500

    started recursive solve, fixed animate

--- Maze.java
+++ Maze.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class Maze{
   private char[][] maze;
   private boolean animate;
+  private int x,y;
 
   public static void main(String[]args){}
   public Maze(){
@@ -24,7 +25,7 @@ public class Maze{
       for(int l=0;l<data.size();l++){
         for(int c=0;c<data.get(l).length();c++){
           maze[l][c] = data.get(l).charAt(c);
-          if(maze[l][c]=='S') countS++;
+          if(maze[l][c]=='S') countS++; x=l;y=c;
           if(maze[l][c]=='E') countE++;
         }
       }
@@ -38,19 +39,50 @@ public class Maze{
     }
   }
 
-  /*Wrapper Solve Function returns the helper function
-    Note the helper function has the same name, but different parameters.
-    Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists. */
-    public int solve(){
-      //find the location of the S.
-
-      //erase the S
-
-      //and start solving at the location of the s.
-
-      //return solve(???,???);
-      return 0;
+  public void setAnimate(boolean b){
+    animate = b;
+  }
+  public void clearTerminal(){
+    //erase terminal, go to top left of screen.
+    System.out.println("\033[2J\033[1;1H");
+  }
+  private void animate(){
+    //automatic animation! You are welcome.
+    if(animate){
+      clearTerminal();
+      System.out.println(this);
+      wait(20);
     }
+  }
+
+  /*Wrapper Solve Function returns the helper function
+  Note the helper function has the same name, but different parameters.
+  Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists. */
+  public int solve(){
+    return solve(x,y);
+  }
+
+  /*
+  Recursive Solve function
+  A solved maze has a path marked with '@' from S to E.
+
+  Returns the number of @ symbols from S to E when the maze is solved,
+  Returns -1 when the maze has no solution.
+
+  Postcondition:
+  The S is replaced with '@' but the 'E' is not.
+  All visited spots that were not part of the solution are changed to '.'
+  All visited spots that are part of the solution are changed to '@'
+  */
+  private int solve(int row, int col){ //you can add more parameters since this is private
+    animate();
+    //COMPLETE SOLVE
+
+    return -1; //so it compiles
+  }
+
+
+
 
   /*Return the string that represents the maze.
   It should look like the text file with some characters replaced. */
